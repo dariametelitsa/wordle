8f10da189e5566655f33a2b821999d3c045f78e4
dariametelitsa <dariametelitsa@gmail.com>
2024-04-27 15:37:50 +0300

    test commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,19 @@
 import React from 'react';
 import './App.css';
+import { Field } from "./components/field/Field";
 
 function App() {
-  return (
-    <div className="App">
-     <h1>Wordle</h1>
-      <p>Some text</p>
-    </div>
-  );
+    return (
+        <div className="App">
+            <h1 className="text-3xl font-bold underline">Wordle</h1>
+            <p className={'text-center text-green-500'}>Some text</p>
+            <p>Additing after added storybook</p>
+            <div className={'flex'}>
+                <Field/>
+            </div>
+
+        </div>
+    );
 }
 
 export default App;
---
+++ src/components/word/Word.tsx
@@ -0,0 +1,13 @@
+// @flow 
+import * as React from 'react';
+
+type Props = {
+    
+};
+export const Word = (props: Props) => {
+    return (
+        <div>
+
+        </div>
+    );
+};
