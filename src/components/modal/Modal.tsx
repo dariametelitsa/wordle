71867f0bd86ea3f1a4488bcf7a7a318480c6ab8a
Daria Metelitsa <dariametelitsa@gmail.com>
2024-07-16 20:33:18 +0300

    added styles for modal

--- src/components/modal/Modal.tsx
+++ src/components/modal/Modal.tsx
@@ -11,15 +11,17 @@ export const Modal = ({isCorrect, turn, solution}: Props) => {
         <div className={`bg-cyan-800 bg-opacity-75 fixed w-screen h-screen top-0 left-0`}>
             {isCorrect && (
                 <div className={'max-w-96 bg-white p-10 rounded shadow-2xl my-52 mx-auto'}>
-                    <h1>You win</h1>
-                    <p>{solution}</p>
+                    <h1 className={'font text-4xl mb-5'}>You win!</h1>
+                    <hr/>
+                    <p className={`font-bold uppercase mt-5`}>{solution}</p>
                     <p>You found the solution in {turn} guesses</p>
                 </div>
             )}
             {!isCorrect && (
-                <div>
-                    <h1>You lose</h1>
-                    <p>{solution}</p>
+                <div className={'max-w-96 bg-white p-10 rounded shadow-2xl my-52 mx-auto'}>
+                    <h1 className={'font text-4xl mb-5'}>So unlucky</h1>
+                    <hr/>
+                    <p className={`font-bold uppercase mt-5`}>{solution}</p>
                     <p>Better luck next time</p>
                 </div>
             )}
