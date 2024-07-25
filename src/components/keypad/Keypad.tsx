ea8e03579c6817ac36a02bc3339d4fe494515a3d
Daria Metelitsa <dariametelitsa@gmail.com>
2024-07-25 18:41:22 +0300

    added function for getting letter

--- src/components/keypad/Keypad.tsx
+++ src/components/keypad/Keypad.tsx
@@ -6,8 +6,9 @@ import { AlphabetType, LetterStatus } from "../../types/Types";
 
 type Props = {
     usedKeys: {[key: string]: LetterStatus}
+    onDigitalLetterPressed: (key: string) => void
 };
-export const Keypad = ({usedKeys}: Props) => {
+export const Keypad = ({usedKeys, onDigitalLetterPressed}: Props) => {
     const [letters, setLetters] = useState<Array<AlphabetType> | null>(null);
 
     useEffect(() => {
@@ -16,13 +17,15 @@ export const Keypad = ({usedKeys}: Props) => {
                 setLetters(res)
             })
     }, []);
+
+
     return (
         <div className={'flex flex-wrap max-w-xl justify-center gap-1'}>
             {letters && letters.map(l => {
 
                 const letterColor = usedKeys[l.key] === 'wrongLetter' ? `bg-gray-200` : usedKeys[l.key] === 'rightPosition' ? `bg-emerald-400` : usedKeys[l.key] === 'wrongPosition' ? `bg-amber-300`: usedKeys[l.key] === 'active' ? 'bg-gray-400 shadow-inner border-2 border-white' : 'bg-gray-400';
 
-                return (<div className={`flex justify-center items-center w-12 h-16 border-2 rounded-2xl border-gray-100 transition ease-in-out delay-300 ${letterColor}`} key={l.key}>{l.key}</div>)
+                return (<div onClick={() => onDigitalLetterPressed(l.key)} className={`flex justify-center items-center w-12 h-16 border-2 rounded-2xl border-gray-100 transition ease-in-out delay-300 ${letterColor}`} key={l.key}>{l.key}</div>)
             })}
         </div>
     );
